fb406270ca3e4fb3a1ff4974e508a8d8b9697c73
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-07 21:04:48 +0900

    #4.3 Events part Two

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(event);
