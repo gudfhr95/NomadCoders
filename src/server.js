7761937d84f5ca2ae93c6a0b851a04fe07886f0e
Hyungrok <ghldnjsdlf@gmail.com>
2021-10-25 22:13:39 +0900

    #3.4 Recap

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("I still love you.");
+  return res.end();
 };
 
 const handleLogin = (req, res) => {
-  return res.send("Login here.");
+  return res.send({ message: "Login here." });
 };
 
 app.get("/", handleHome);
