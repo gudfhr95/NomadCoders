e47aefe0aa635137cc4081572b93d08db2cd36a6
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-10-31 23:48:00 +0900

    #7.5 Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,12 +18,34 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({ name, username, email, password, location });
+  try {
+    await User.create({ name, username, email, password, location });
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "UploadVideo",
+      errorMessage: error._message,
+    });
+  }
   return res.redirect("/login");
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  // check if password correct
+  res.end();
 };
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
