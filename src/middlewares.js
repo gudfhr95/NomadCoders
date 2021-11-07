6e1b956e1a542232de29a74812086bf23662d349
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-11-07 19:57:16 +0900

    #17.9 Production Environment

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,16 +9,25 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "wetube-gudfhr95",
+  bucket: "wetube-gudfhr95/images",
   acl: "public-read",
 });
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-gudfhr95/videos",
+  acl: "public-read",
+});
+
+const isHeroku = process.env.NODE_ENV === "production";
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -45,7 +54,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -53,5 +62,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
--- src/server.js
+++ src/server.js
@@ -28,11 +28,6 @@ app.use(
 
 app.use(flash());
 app.use(localsMiddleware);
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
 
 app.use("/uploads", express.static("uploads"));
 app.use(
